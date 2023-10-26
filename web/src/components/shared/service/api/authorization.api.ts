449d4e04cf7040fbd661302776f8160f83daed08
Vilija Mažeikė <vmazeike@reiz.tech>
2023-10-26 12:17:23 +0300

    update data type

--- web/src/components/shared/service/api/authorization.api.ts
+++ web/src/components/shared/service/api/authorization.api.ts
@@ -1,5 +1,9 @@
 import { AxiosRequestConfig } from 'axios';
-import { LoginForm } from '../../../pages/authPage/hooks/useLogin';
+
+export interface LoginForm {
+  email: string;
+  password: string;
+}
 
 export const login = (data: LoginForm): Partial<AxiosRequestConfig> => ({
   url: 'http://localhost:7000/login',
