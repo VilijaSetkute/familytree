d53fbf4a6e04ad5c2310bc1bd871bc1b311a451c
Vilija Mažeikė <vmazeike@reiz.tech>
2023-11-01 11:58:15 +0200

    update response messages

--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -28,24 +28,20 @@ module.exports.Login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
     if (!email || !password) {
-      return res.json({ message: "All fields are required" });
+      return res.status(400).json({ message: "All fields are required" });
     }
     const user = await User.findOne({ email });
     if (!user) {
-      return res
-        .status(400)
-        .json({ message: `User not found with email ${email}` });
+      return res.status(400).json({ message: `Login failed` });
     }
     const auth = await bcrypt.compare(password, user.password);
     if (!auth) {
-      return res
-        .status(400)
-        .json({ message: `Incorrect password for email ${email}` });
+      return res.status(400).json({ message: `Login failed` });
     }
     if (user && auth && !user.accountActivated) {
       return res
         .status(400)
-        .json({ message: "Your identity has not been confirmed yet" });
+        .json({ message: "Your account has not been activated yet" });
     }
     const token = createSecretToken(user._id);
     res.cookie("token", token, {
