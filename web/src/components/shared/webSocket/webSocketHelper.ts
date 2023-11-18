73e006cfdc4619b02a3e4d60f9394ee9b335d438
Vilija Mažeikė <vmazeike@reiz.tech>
2023-11-18 15:56:53 +0200

    update websocket helper

--- web/src/components/shared/webSocket/webSocketHelper.ts
+++ web/src/components/shared/webSocket/webSocketHelper.ts
@@ -1,7 +1,4 @@
-import io from 'socket.io-client';
 const { REACT_APP_PUBLIC_SERVER_URL, REACT_APP_LOCAL_SERVER_URL, REACT_APP_PRODUCTION } = process.env;
 
-const socketOrigin: string =
+export const socketOrigin: string =
   REACT_APP_PRODUCTION === 'true' ? (REACT_APP_PUBLIC_SERVER_URL as string) : (REACT_APP_LOCAL_SERVER_URL as string);
-
-export const socket = io(socketOrigin as string);
